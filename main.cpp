18ee13929d016a97c829530e8625751791d8b950
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-03-27 20:16:23 -0300

    ocupacao de bytes

--- main.cpp
+++ main.cpp
@@ -3,20 +3,10 @@
 
 int main(){ 
 
- int a = 8;
- //int número inteiro
- float b = 9.6;
- // número de ponto flutuante ocupa 4bytes
- double c = 4.6238476583;
- //double ocupa 8bytes
- 
- std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
-
-
- std::cout<<a<<std::endl;
- std::cout<<b<<std::endl;
- std::cout<<c<<std::endl;
-
+std::cout<<8+2<<std::endl;
+std::cout<<10+4<<std::endl;
+std::cout<<4*3<<std::endl;
+std::cout<<8/2<<std::endl;
 
         return 0;
 }
