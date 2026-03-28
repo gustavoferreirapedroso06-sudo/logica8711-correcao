ed67ca7936e68766e1b586b26b8182562c6f55ed
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-03-27 21:54:48 -0300

    identidade

--- main.cpp
+++ main.cpp
@@ -4,13 +4,32 @@
 int main(){ 
 
 std::string nome;
+int idade;
+float altura;
 
 
 
-std::cout<<"qual seu nome: "<<std::endl;
-std::cin>>nome;
+        std::cout<<"qual seu nome: "<<std::endl;
+        std::cin>>nome;
 
-std::cout<<"Bem vindo(a) "<<nome<<"!!"<<std::endl;
+        std::cout<<"Bem vindo(a) "<<nome<<"!!"<<std::endl;
+
+
+
+        std::cout<<"Qual sua idade: "<<std::endl;
+        std::cin>>idade;
+
+        std::cout<<"Sua idade e: "<<idade<<"!!"<<std::endl;
+
+
+
+        std::cout<<"qual sua altura?: "<<std::endl;
+        std::cin>>altura;
+
+        std::cout<<"sua altura e: "<<altura<<"!!"<<std::endl;
+
+
+
 
 
         return 0;
