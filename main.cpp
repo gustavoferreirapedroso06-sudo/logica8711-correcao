d189b464d666573a45b2de06eeec0aa7df2fa6cc
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-03-30 20:46:16 -0300

    valor de A e B

--- main.cpp
+++ main.cpp
@@ -14,9 +14,10 @@ std::cin>>a;
 std::cout<<"Digite um valor para b: "<<std::endl;
 std::cin>>b;
 
-std::cout<<(a > b)<<std::endl;
-std::cout<<( a >= b )<<std::endl;
-std::cout<<(a != b)<<std::endl;
+
+std::cout<<(a != b )<<std::endl;
+std::cout<<(a == b )<<std::endl;
+
 
         return 0;
 }
