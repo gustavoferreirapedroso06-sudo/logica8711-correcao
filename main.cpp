472ebe57c2644c8eaba53fb4ea43ce103ba5d786
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-01 21:11:13 -0300

    soma das variaveis 2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@ int main(){
 
 int result1 = 4 + 4 *2;
 int result2 = 4 + 2 / 2;
+int result3 = (4 + 4) *2;
 
 std::cout<<"o resultado numero 1 e: "<<result1<<std::endl;
 std::cout<<"o resultado numero 2 e: "<<result2<<std::endl;
+std::cout<<"o resultado numero 3 e: "<<result3<<std::endl;
+
 
 
 
