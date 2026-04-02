954ea1ed91fc5d3707d08b23bc4c8ec8e069d1ca
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-01 21:57:56 -0300

    nome e sobrenome

--- main.cpp
+++ main.cpp
@@ -3,19 +3,10 @@
 
 int main(){ 
 
-int num1, num2, soma;
+std::string nome = "Gustavo";
+std::string sobrenome = "ferreira";
 
-std::cout<<"Digite num 1: "<<"\n";
-std::cin>>num1;
-
-std::cout<<"Digite num 2: "<<"\n";
-std::cin>>num2;
-
-soma = num1 + num2;
-
-
-std::cout<<"O resultado da soma é "<<soma<<"\n";
-
+std::cout<<nome + " " + sobrenome<<std::endl;
 
 
 
