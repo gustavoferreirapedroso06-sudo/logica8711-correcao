45e316d60304171ba5cff42f6018e463a04e5cd4
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-06 21:56:23 -0300

    nome.at

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ std::cout<<nome[2]<<std::endl;
 std::cout<<nome[nome.length() - 1]<<std::endl;
 nome[0] = 'A';
 std::cout<<nome<<std::endl;
+std::cout<<nome.at(0)<<std::endl;
+nome.at(0) = 'H';
+std::cout<<nome;
 
         return 0;
 }
