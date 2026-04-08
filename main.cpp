c303b3d995e191df2be57e55e1eb202162ba2279
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-08 20:40:59 -0300

    comparacao de valores

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 
 int main(){ 
 
-int a = 5;
+int a = 100;
 int b = 7;
+int c = 6;
+int d = 10;
 
+int maior1 = std::max(a, b);
+int maior2 = std::max(c, d);
 
-std::cout<<std::min(a, b)<<std::endl;
-std::cout<<std::max(a, b)<<std::endl;
-              
-
+std::cout<<(maior1 > maior2)<<std::endl;
 
         return 0;
 }
