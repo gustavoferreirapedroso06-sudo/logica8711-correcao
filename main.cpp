8d107174c19e450dca2c4196c73cf7d4845531b0
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-13 19:51:18 -0300

    if e else verificaçao de idade 03

--- main.cpp
+++ main.cpp
@@ -14,6 +14,12 @@ if(idade < 0){
         std::cout<<"Voce e um bebe! "<<std::endl;
 } else if(idade <= 8 ){
         std::cout<<"Voce e uma crianca!"<<std::endl;
+} else if(idade <=16){
+        std::cout<<"Voce e um(a) adolescente!"<<std::endl;
+} else if(idade <=60){
+        std::cout<<"Voce e adulto(a)!"<<std::endl;
+} else{
+        std::cout<<"Voce e um(a) senior!!"<<std::endl;
 }
 
 
