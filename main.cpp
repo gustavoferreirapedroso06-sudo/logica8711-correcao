e487324a496b44acd2c47a0504d67ce60caa7d61
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-15 20:22:39 -0300

    switch para dia da semana (opções)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,47 @@
-#include<iostream>
+#include <iostream>
 
-int main(){ 
+int main()
+{
 
-   int tabuada = 7;
 
-   for(int i = 0; i <= 10; i++){
-        std::cout<<"o resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+          int opcao;
 
-   }
+        std::cout << "Digite um valor para opção: " << std::endl;
+        std::cin >> opcao;
+
+        switch (opcao){
+                  case 1:
+                std::cout << "Domingo!" << std::endl;
+                break;
+        case 2:
+                std::cout << "Segunda-Feira!" << std::endl;
+                break;
+        case 3:
+                std::cout << "terça-Feira!" << std::endl;
+                break;
+        case 4:
+                std::cout << "Quartra-Feira!" << std::endl;
+                break;
+        case 5:
+                std::cout << "Quinta-Feira!" << std::endl;
+                break;
+        case 6:
+                std::cout << "Sexta-Feira!" << std::endl;
+                break;
+        case 7:
+                std::cout << "Sabado!" << std::endl;
+                break;
+        default:
+                std::cout << "Numero invalido!" << std::endl;
+        }
+        
+
+      
+        
 
 
 
         return 0;
 }
+
+      
