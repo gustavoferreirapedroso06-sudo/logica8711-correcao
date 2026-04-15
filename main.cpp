87250bb58fe898ca9bbcbe93a702fcabb075cc10
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-15 19:43:04 -0300

    contagem regressiva com segundos

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
+#include<thread>//para lidar com o tempo de execução
+#include<chrono>//para definir unidades de tempo (ms, segundos)
 
 int main(){ 
 
 for(int i = 10; i >= 0;i--){
         std::cout<<"a bomba ira explodir em.. "<<i<<std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
 }
+
+
 
         return 0;
 }
