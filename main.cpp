80dd30cb64839a0a52c5b6f8d60a1d87d7fefeb8
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-15 21:42:35 -0300

    array com for int 002

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
-int main(){
+int main()
+{
 
-        int myNumbers [5] = {10, 20, 30 ,40 ,50};
-        for (int num: myNumbers){
-        std::cout<<num<<std::endl;
-        
+        int myNumbers[5] = {10, 20, 30, 40, 50};
+        // for (int num : myNumbers)
+        // {
+        //         std::cout << num << std::endl;
+        for(int i = 0; i < 5; ++i){
+                std::cout<<myNumbers[1]<<std::endl;
         }
+        // }
 
         return 0;
 }
