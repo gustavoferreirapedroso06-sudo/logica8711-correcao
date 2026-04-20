41ad58c5991b548f219cc101ed8869e71e9eb3a1
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-20 19:41:53 -0300

    array puxar idade tabela 002

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 int main()
 {
 
-        std::string nomes [3] = {"João", "paula" ,"murilo"};
+        std::string nomes [3] = {"Joao", "paula" ,"murilo"};
         int idades [3] = {27, 31, 20};
 
-        std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos!"<<std::endl;
+
+       std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos!!"<<std::endl;
+       std::cout<<nomes[2]<<" tem "<<idades[2]<<" anos!!"<<std::endl;
+       std::cout<<nomes[0]<<" tem "<<idades[0]<<" anos!!"<<std::endl;
+
 
 
 
