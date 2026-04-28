cd923b5e1c43f8337b663b188eb75856b69a9efd
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-28 20:03:07 -0300

    diferença do array

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-#include <vector>
+
 
 
 int main()
 {
 
-       int myNumbers[5] = {1,2,3,4,5};
+      int myNumbers[5] = {1,2,3,4,5};
 
-       for(int i = 0; i <5; i++){
-        std::cout<<myNumbers[1]<<std::endl;
-       }
+    //   for(int i = 0; i <5; i++){
+   //     std::cout<<myNumbers[1]<<std::endl;
+  //     }
+
+  for(int num : myNumbers){
+        std::cout<<num<<std::endl;
+  }
 
      return 0;
 }
