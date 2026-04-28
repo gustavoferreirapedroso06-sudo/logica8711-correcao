d311748c364ebedc601c998e917da872dc8f7936
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-28 20:12:13 -0300

    void myfunction

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 
-
+void myFunction(){
+        std::cout<<"ola mundo!"<<std::endl;
+}
 
 int main()
 {
 
-      int myNumbers[5] = {1,2,3,4,5};
-
-    //   for(int i = 0; i <5; i++){
-   //     std::cout<<myNumbers[1]<<std::endl;
-  //     }
-
-  for(int num : myNumbers){
-        std::cout<<num<<std::endl;
-  }
+        myFunction();
 
      return 0;
 }
