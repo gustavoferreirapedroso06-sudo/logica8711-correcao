a5dadd8750d957e2af9420ac9315d5e5db2d52b2
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-04-28 21:07:32 -0300

    void nome e idade

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <string>
 
-void minhaFuncao(std::string nome){
-        std::cout<<nome<<std::endl;
+void minhaFuncao(std::string nome, int idade){
+        std::cout<<"Meu nome e "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
 
 
 int main()
 {
 
-minhaFuncao("Gustavo");
-minhaFuncao("Nome");
+minhaFuncao("Gustavo", 38);
+
 
      return 0;
 }
